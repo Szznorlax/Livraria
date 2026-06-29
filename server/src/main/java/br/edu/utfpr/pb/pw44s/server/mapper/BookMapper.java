fc73b43aaefca22942f0bd5350b38a35c17251fb
Szznorlax <maycon344@yahoo.com>
2026-06-29 18:27:06 -0300

    Updated create book endpoint to include author

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/mapper/BookMapper.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/mapper/BookMapper.java
@@ -3,13 +3,16 @@ package br.edu.utfpr.pb.pw44s.server.mapper;
 
 import br.edu.utfpr.pb.pw44s.server.dto.BookDTO;
 import br.edu.utfpr.pb.pw44s.server.model.Book;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CategoryMapper.class)
 public interface BookMapper {
 
+    @Mapping(target = "author", source = "author")
     BookDTO toDto(Book entity);
 
+    @Mapping(target = "author", source = "author")
     Book toEntity(BookDTO dto);
 }
